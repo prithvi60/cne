dbf49b5313a6df7c886e9d12b481151defb7c30a
Prithvi <prithvin19@gmail.com>
2024-03-13 18:17:11 +0530

    phone fix needed

--- app/page.js
+++ app/page.js
@@ -4,7 +4,7 @@ import { AyurvedaBanner } from "@/components/AyurvedaBanner";
 import { Evolution } from "@/components/Evolution";
 import { Global } from "@/components/Global";
 import { Hero } from "@/components/Hero";
-import { Legacy } from "@/components/Legacy";
+import Legacy from "@/components/Legacy";
 import { Legacy_Products } from "@/components/Legacy_Products";
 import Newsletter from "@/components/Newsletter";
 import { Our_Brands } from "@/components/Our_Brands";
